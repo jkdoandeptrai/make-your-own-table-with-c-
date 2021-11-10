a6bf7d721022cf118da4e21f93d8a1aa238675ea
maingocdoan <88520961+jkdoandeptrai@users.noreply.github.com>
2021-11-10 09:37:06 +0700

    commit: prettytable
    my code seems a bit messy but it works, hope you like it. I'll try to make it more readable in the future. Thank you!

---
+++ Prettytable.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+class Prettytable{
+   private:
+      vector<vector<string>> m_row;
+      vector<string> m_head;
+      vector<int> sizeCol;
+      int length = 0;
+      // phan chia cot:
+      void findSize(){
+         int size = m_head.size();
+         int sizeR = m_row.size();
+         for ( int r = 0; r < size; r ++){ // cot
+            int max = sizeCol[r];
+            for ( int i = 0; i < sizeR; i++){ // hang
+               if ( m_row[i][r].length() > max ){
+                  max = m_row[i][r].length();
+               }
+            }
+            sizeCol[r] = max;
+            length += max;
+         }
+      }
+   public:
+      Prettytable(vector<string> yourHeader){
+         for ( auto&& i : yourHeader){
+            m_head.push_back(i);
+            sizeCol.push_back(i.length());
+         }
+      }
+      void addRow(vector<string> row){
+         m_row.push_back(row);
+      }
+      void printTable(){
+         findSize();
+         length = length + 6*m_head.size() + 1;
+         // dau
+         cout << "+";
+         for ( int i = 0; i < length; i ++ ){
+            cout << "-";
+         }
+         cout << "+";
+         cout << endl;
+         cout << "|";
+         // header
+         for (int i = 0; i < m_head.size(); i++ ){
+            cout <<"  " << left <<setw(sizeCol[i] + 3) << m_head[i] << "| ";
+         }
+         cout << endl;
+         // than
+         for ( int m = 0; m < m_row.size(); m ++){
+            cout << "+";
+            for ( int i = 0; i < length; i ++ ){
+               cout << "-";
+            }
+            cout << "+";
+            cout << endl;
+            cout << "|";
+            for (int i = 0; i < m_head.size(); i++ ){
+               cout <<"  " << left <<setw(sizeCol[i] + 3) << m_row[m][i] << "| ";
+            }
+            cout << endl;
+         }
+         // cuoi
+         cout << "+";
+         for ( int i = 0; i < length; i ++ ){
+            cout << "-";
+         }
+         cout << "+";
+         cout << endl;
+         return;
+      }
+};
